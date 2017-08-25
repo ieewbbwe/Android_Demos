bc16bb4e579e4c5daafc3103aca7d7f0f2f6812a
webber <iewebber@126.com>
2017-08-25 17:56:55 +0800

    添加Socket实例

--- app/src/main/java/com/webber/demos/trandfer/socket/SocketActivity.java
+++ app/src/main/java/com/webber/demos/trandfer/socket/SocketActivity.java
@@ -1,99 +1,173 @@
 package com.webber.demos.trandfer.socket;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.webber.demos.R;
 
-import java.io.FileInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.UnknownHostException;
+import java.util.concurrent.Executors;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class SocketActivity extends AppCompatActivity implements ISocket {
+public class SocketActivity extends AppCompatActivity implements View.OnClickListener {
 
-    @Bind(R.id.m_client_bt)
-    Button mClientBt;
-    @Bind(R.id.m_service_bt)
-    Button mServiceBt;
+    @Bind(R.id.m_start_bt)
+    Button mStartBt;
+    @Bind(R.id.m_close_bt)
+    Button mCloseBt;
+    @Bind(R.id.m_message_et)
+    EditText mMessageEt;
+    @Bind(R.id.m_send_bt)
+    Button mSendBt;
     @Bind(R.id.m_test_tv)
     TextView mTestTv;
+    private String HOST = "10.27.0.197";
+    private int PORT = 1993;
+    private Socket mSocket;
+    private Handler handler = new Handler(new Handler.Callback() {
+        @Override
+        public boolean handleMessage(Message msg) {
+            switch (msg.what) {
+                case 1:
+                    Log.d("picher_log", "接收到数据：" + (String) msg.obj);
+                    mTestTv.setText(mTestTv.getText() + "\n" + msg.obj);
+                    break;
+            }
+            return false;
+        }
+    });
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_socket);
         ButterKnife.bind(this);
+
+        init();
     }
 
-    @Override
-    public void openSocketClientChannel() {
-        Socket socket;
-        try {// 创建一个Socket对象，并指定服务端的IP及端口号
-            socket = new Socket("192.168.1.32", 1989);
-            // 创建一个InputStream用户读取要发送的文件。
-            InputStream inputStream = new FileInputStream("e://a.txt");
-            // 获取Socket的OutputStream对象用于发送数据。
-            OutputStream outputStream = socket.getOutputStream();
-            // 创建一个byte类型的buffer字节数组，用于存放读取的本地文件
-            byte buffer[] = new byte[4 * 1024];
-            int temp = 0;
-            // 循环读取文件
-            while ((temp = inputStream.read(buffer)) != -1) {
-                // 把数据写入到OuputStream对象中
-                outputStream.write(buffer, 0, temp);
-            }
-            // 发送读取的数据到服务端
-            outputStream.flush();
-
-            /** 或创建一个报文，使用BufferedWriter写入,看你的需求 **/
-//          String socketData = "[2143213;21343fjks;213]";
-//          BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(
-//                  socket.getOutputStream()));
-//          writer.write(socketData.replace("\n", " ") + "\n");
-//          writer.flush();
-            /************************************************/
-        } catch (UnknownHostException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    private void init() {
+        mStartBt.setOnClickListener(this);
+        mCloseBt.setOnClickListener(this);
+        mSendBt.setOnClickListener(this);
     }
 
-    @Override
-    public void openSocketServiceChannel() {
-        // 声明一个ServerSocket对象
-        ServerSocket serverSocket = null;
+    private void openSocketClientChannel() {
         try {
-            // 创建一个ServerSocket对象，并让这个Socket在1989端口监听
-            serverSocket = new ServerSocket(1989);
-            // 调用ServerSocket的accept()方法，接受客户端所发送的请求，
-            // 如果客户端没有发送数据，那么该线程就停滞不继续
-            Socket socket = serverSocket.accept();
-            // 从Socket当中得到InputStream对象
-            InputStream inputStream = socket.getInputStream();
-            byte buffer[] = new byte[1024 * 4];
-            int temp = 0;
-            // 从InputStream当中读取客户端所发送的数据
-            while ((temp = inputStream.read(buffer)) != -1) {
-                System.out.println(new String(buffer, 0, temp));
+            if (mSocket == null) {
+                //1 创建Socket实例打开通道
+                mSocket = new Socket(HOST, PORT);
+                Log.d("picher_log", "Socket开启！");
+                //2 创建读数据线程，从Socket中读出数据
+                new Thread(new ReadHandlerThread()).start();
             }
-            serverSocket.close();
         } catch (IOException e) {
             e.printStackTrace();
+            try {
+                mSocket.close();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
         }
     }
 
     @Override
-    public void transferData(OutputStream outputStream) {
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.m_start_bt:
+                Executors.newSingleThreadExecutor().submit(new Runnable() {
+                    @Override
+                    public void run() {
+                        openSocketClientChannel();
+                    }
+                });
+                break;
+            case R.id.m_stop_bt:
+                closeSocketClientChannel();
+                break;
+            case R.id.m_send_bt:
+                sendSocketData();
+                break;
+        }
+    }
 
+    public void sendSocketData() {
+        new Thread(new WriteHandlerThread()).start();
+    }
+
+    private class ReadHandlerThread implements Runnable {
+
+        @Override
+        public void run() {
+            DataInputStream inputStream;
+            try {
+                //3 向Socket中发送数据
+                inputStream = new DataInputStream(mSocket.getInputStream());
+                String receiver = inputStream.readUTF();
+                handler.obtainMessage(1, receiver).sendToTarget();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private class WriteHandlerThread implements Runnable {
+
+        @Override
+        public void run() {
+            InputStream inputStream = null;
+            OutputStream outputStream = null;
+            String str = mMessageEt.getText().toString().trim();
+            Log.d("picher_log", "发送的数据是:：" + str);
+            try {
+                outputStream = mSocket.getOutputStream();
+                inputStream = new ByteArrayInputStream(str.getBytes());
+                byte[] buffer = new byte[1024 * 4];
+                int temp = 0;
+                while ((temp = inputStream.read(buffer)) != -1) {
+                    outputStream.write(buffer, 0, temp);
+                }
+                outputStream.flush();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    if (outputStream != null) {
+                        outputStream.close();
+                    }
+                    if (inputStream != null) {
+                        inputStream.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private void closeSocketClientChannel() {
+        if (mSocket != null) {
+            try {
+                mSocket.close();
+                Log.d("picher_log", "Socket已关闭！");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
