51f136bd50db7e4aba33a6f5652bd77ae77cb720
picher <ieewbber@gmail.com>
2018-03-06 16:17:57 +0800

    some demos add

---
+++ app/src/main/java/com/webber/demos/media/MediaControl.java
@@ -0,0 +1,9 @@
+package com.webber.demos.media;
+
+/**
+ * Created by picher on 2018/3/1.
+ * Describe：
+ */
+
+interface MediaControl {
+}
---
+++ app/src/main/java/com/webber/demos/media/MediaDemoActivity.java
@@ -0,0 +1,13 @@
+package com.webber.demos.media;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MediaDemoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_media_demo);
+    }
+}
